c90c083bb4becdad00ab78bf72abbeb1aa3be4ec
ellenfranceszakreski <efzakreski@mac.com>
2019-10-06 09:47:36 -0400

    Update remake_cicjobs.sh

--- remake_cicjobs.sh
+++ remake_cicjobs.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # remake_cicjobs.sh <JobName> <optional subject list file>
-# e.g. ./remake_cicjobs.sh smooth06
+# e.g. ./remake_cicjobs.sh prepro
 # make cic job file for each subject and list of cic jobs (cicjobs/cicjoblist) and test job list
 # required: Scripts/subjects.txt
 # required: Scripts/<JobName>_job_template.m
 # <JobName>_job_template.m must be MATLAB 2012a compatible '.m' script that depends on a variable subx (subject name).
 
 # analysis directory
-AnalysisDir=/data/scratch/zakell/sept19fmri
+AnalysisDir=/data/scratch/zakell/fmri_oct2019
 if [ ! -d $AnalysisDir ]; then
 	echo "error: Could not find analysis directory at "$AnalysisDir
 	unset AnalysisDir
