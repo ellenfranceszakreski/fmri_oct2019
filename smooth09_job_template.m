602b96a4fb065181b609f1c686f6cacc1b2e0fee
ellenfranceszakreski <efzakreski@mac.com>
2019-10-08 14:20:40 -0400

    Create smooth09_job_template.m

---
+++ smooth09_job_template.m
@@ -0,0 +1,30 @@
+% smooth09_job_template
+% append code after defining this variable: subx, runx
+% assumes data has already been normalized
+% NOTE: MUST DEFINE SUBJECT AND RUN!!!!
+% e.g. subx = 'sub1'
+% e.g. runx = 'run1'         f = spm_select('ExtFPList',dirname,anat_filter);
+number_of_cores=12;
+d=tempname();% get temporary directory location
+mkdir(d);
+% create cluster
+cluster = parallel.cluster.Local('JobStorageLocation',d,'NumWorkers',number_of_cores);
+matlabpool(cluster, number_of_cores);
+%% analysis
+addpath('/data/scratch/zakell/fmri_oct2019/Scripts');
+addpath(genpath(fullfile(spm('dir'),'config')));
+subxDir=['/data/scratch/zakell/fmri_oct2019/Input/',subx];
+
+matlabbatch = {};
+matlabbatch{1}.spm.spatial.smooth.data = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.smooth.data(1) = cellstr(spm_select('ExtFPList', subxDir, ['^wausub\d+_',runx,'.nii'], 1:200));
+matlabbatch{1}.spm.spatial.smooth.fwhm = [9 9 9];
+matlabbatch{1}.spm.spatial.smooth.dtype = 0;
+matlabbatch{1}.spm.spatial.smooth.im = 0;
+matlabbatch{1}.spm.spatial.smooth.prefix = 's09';
+
+spm('defaults', 'FMRI');
+spm_jobman('run', matlabbatch);
+% save file in subject's directory to indicate successful job completion
+save(fullfile(subxDir,'smooth09_done.mat'), 'matlabbatch','-mat');
+% done
