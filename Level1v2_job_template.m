a95a5c7d6a9d1caae22e5499c6b9a838ec7bcbd6
ellenfranceszakreski <efzakreski@mac.com>
2019-10-08 16:20:40 -0400

    Update Level1v2_job_template.m

--- Level1v2_job_template.m
+++ Level1v2_job_template.m
@@ -2,6 +2,8 @@
 % append this code after defining variable subx, e.g. subx='sub2';
 % subject should be smoothed already.
 
+AnalysisDir='/data/scratch/zakell/fmri_oct2019'; %<-make sure this is correct
+
 %% set up cluster
 number_of_cores=12;
 d=tempname();% get temporary directory location
@@ -15,9 +17,6 @@ matlabpool(cluster, number_of_cores);
 addpath([AnalysisDir,'/Scripts']);
 addpath(genpath([spm('dir'),'/config']));
 
-subxDir = prepare_subx(subx);clear subx % .m file is in Scripts folder (deletes pre-existing directory for this subject if it exists);
-
-AnalysisDir='/data/scratch/zakell/fmri_oct2019';
 % List of open inputs
 % Named Directory Selector: Directory - cfg_files
 jobs = {[AnalysisDir,'/Scripts/Level1v2_job.m']};
