729db1d99d899d2b139468c139c6fc6a1276ff33
ellenfranceszakreski <efzakreski@mac.com>
2019-10-12 13:03:44 -0400

    Update clone_github.sh

--- clone_github.sh
+++ clone_github.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
-# e.g. ./clone_github.sh clone github to directory Github (on analysis path)
+# ./clone_github.sh clone github files to CIC directory Github (on analysis path)
 # get code from git hub repository named 'fmri_oct2019'
 
-AnalysisDir=/data/scratch/zakell/fmri_oct2019
+AnalysisDir=/data/scratch/zakell/fmri_oct2019 # make sure this is correct (name should not have any spaces)
+
+# make Scripts directory if one does not already exist
+test ! -d $AnalysisDir/Scripts && mkdir $AnalysisDir/Scripts
 GithubDir=$AnalysisDir/Github
 
 # remake Github directory
 test -d $GithubDir && rm -r $GithubDir
 mkdir $GithubDir
 
-echo "Cloning github repository fmri_oct2019 to $GithubDir"
-
-## clone github files
+# clone github files
 git clone https://github.com/ellenfranceszakreski/fmri_oct2019 --depth 1 --branch=master $GithubDir
-echo "Done cloning"
 
-## make scripts executable and easy to delete (github sets access of files clones to read only)
-echo "resetting access of cloned files"
+# make scripts executable and easy to delete (github sets access of files clones to read only)
 chmod -R 777 "$GithubDir"
-echo "New files"
+
+# show new files
+echo "New files:"
 ls -l $GithubDir
+
+# print instructions for moving to Scripts directory
 printf "\n\nTransfer done. When ready, enter commands below:\n"
 printf "cd %s\n" "$AnalysisDir"
 printf "mv -v Github/* Scripts\n\n"
 unset GithubDir AnalysisDir
+exit 0
 ### DONE
